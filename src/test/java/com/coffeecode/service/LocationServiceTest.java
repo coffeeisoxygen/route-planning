973fd0f2db1b8c99ef135f78b512952a49749924
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-20 22:21:37 +0700

    Remove redundant MockitoAnnotations setup in LocationServiceTest

--- src/test/java/com/coffeecode/service/LocationServiceTest.java
+++ src/test/java/com/coffeecode/service/LocationServiceTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.coffeecode.model.Locations;
@@ -29,10 +30,6 @@ class LocationServiceTest {
     @InjectMocks
     private LocationService locationService;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     void testAddLocation() {
