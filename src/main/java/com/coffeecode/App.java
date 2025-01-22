128a31ef8a6c43f47057c61dd50316ca02e33cb8
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-22 10:26:03 +0700

    just finished fist refactoring

--- src/main/java/com/coffeecode/App.java
+++ src/main/java/com/coffeecode/App.java
@@ -2,10 +2,17 @@ package com.coffeecode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.coffeecode.application.service.LocationServiceImpl;
+import com.coffeecode.application.service.RouteServiceImpl;
 import com.coffeecode.config.AppConfig;
+import com.coffeecode.domain.model.Locations;
+import com.coffeecode.domain.model.Route;
 import com.coffeecode.presentation.view.MainFrame;
+
+import java.util.Collection;
 
 import javax.swing.SwingUtilities;
 
@@ -18,6 +25,9 @@ public class App {
             // Create Spring context with both configs
             AnnotationConfigApplicationContext context
                     = new AnnotationConfigApplicationContext(AppConfig.class);
+
+            // Run simulation
+            runSimulation(context);
 
             SwingUtilities.invokeLater(() -> {
                 MainFrame mainFrame = context.getBean(MainFrame.class);
@@ -37,5 +47,32 @@ public class App {
             logger.error("Application failed to start", e);
             System.exit(1);
         }
+    }
+
+    private static void runSimulation(ApplicationContext context) {
+        LocationServiceImpl locationService = context.getBean(LocationServiceImpl.class);
+        RouteServiceImpl routeService = context.getBean(RouteServiceImpl.class);
+
+        // Add test locations
+        logger.info("Starting simulation...");
+
+        Locations bandung = locationService.addLocation("Bandung", -6.914744, 107.609810);
+        Locations jakarta = locationService.addLocation("Jakarta", -6.200000, 106.816666);
+        Locations surabaya = locationService.addLocation("Surabaya", -7.250445, 112.768845);
+
+        logger.info("Added locations: Bandung, Jakarta, Surabaya");
+
+        // Get routes
+        Route jakartaBandung = routeService.getRoute(jakarta.id(), bandung.id());
+        Route jakartaSurabaya = routeService.getRoute(jakarta.id(), surabaya.id());
+
+        logger.info("Route Jakarta-Bandung: {} km", jakartaBandung.distance());
+        logger.info("Route Jakarta-Surabaya: {} km", jakartaSurabaya.distance());
+
+        // Get all routes
+        Collection<Route> allRoutes = routeService.getAllRoutes();
+        logger.info("Total routes in system: {}", allRoutes.size());
+
+        logger.info("Simulation completed successfully");
     }
 }
